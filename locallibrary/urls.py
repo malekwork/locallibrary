0dcc382978b508b4416f0b8add30f2d7a0c5ad3a
malekwork <malek.person@gimail.com>
2024-05-31 05:09:12 -0700

    created root templates. added auth. added login.html

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, auth
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,3 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls'))
+]
