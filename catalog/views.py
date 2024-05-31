6d676408dde732cb99a20bd0fafc0a947bceabe0
malekwork <malek.person@gimail.com>
2024-05-30 23:52:40 -0700

    replace view.index() with generic.TemplateView

--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 
+class HomePageView(generic.TemplateView):
+    template_name = "catalog/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['num_books'] = Book.objects.all().count()
+        context['num_instances'] = BookInstance.objects.all().count()
+        context['num_instances_available'] = BookInstance.objects.filter(status__exact='a').count()
+        context['num_authors'] = Author.objects.count()
+        return context
+
+
+"""
 def index(request):
-    """
-    View function for home page of site.
-    """
-    # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    num_authors = Author.objects.count()  # The 'all()' is implied by default.
+    num_authors = Author.objects.count() 
 
-    # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'catalog/index.html',
@@ -21,3 +29,4 @@ def index(request):
                  'num_instances': num_instances,
                  'num_instances_available': num_instances_available,
                  'num_authors': num_authors})
+"""
