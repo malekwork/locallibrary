cf01f195f2a92c009bfca06aff06ff1c14e8fedf
malekwork <malek.person@gimail.com>
2024-05-31 10:53:18 -0700

    registration url and templates added

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -14,16 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin, auth
+
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('catalog/', include('catalog.urls')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls'))
+    path("accounts/", include('django.contrib.auth.urls'))
 ]
