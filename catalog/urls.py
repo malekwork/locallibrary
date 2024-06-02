1897eaf84f86e13ce24e9871453ed42aedee992b
malekwork <malek.person@gimail.com>
2024-06-02 15:06:45 -0700

    created a form for book renew and dependeces

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import HomePageView, BookListView, BookDetailView, AuthorListView, LoanedBooksByUserListView
+from .views import HomePageView, BookListView, BookDetailView, AuthorListView, LoanedBooksByUserListView,\
+    renew_book_librarian
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='index'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('book/<pk>/', BookDetailView.as_view(), name='book_detail'),
     path('author/', AuthorListView.as_view(), name='author_list'),
     path('mybooks/', LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('book/<pk>/renew/', renew_book_librarian, name='renew-book-librarian')
 ]
